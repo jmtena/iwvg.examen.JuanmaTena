3387434ca73a09cb68a18f9710b1a902b492c4eb
JTENAESCOB001 <jm.tena01@hotmail.com>
2016-01-15 22:26:40 +0100

    Udpate without error

--- src/main/java/iwvg/prac2/controllers/MoveController.java
+++ src/main/java/iwvg/prac2/controllers/MoveController.java
@@ -1,7 +1,6 @@
 package iwvg.prac2.controllers;
 
 import iwvg.prac2.models.Game;
-import iwvg.prac2.utils.Option;
 
 public abstract class MoveController extends Controller implements MoveOperationController{
 	
